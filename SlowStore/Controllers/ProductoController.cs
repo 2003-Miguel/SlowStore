a886d930861ca04ec199ac3a3f9692373de689f3
Miguel <miguelalejandrovasqueslara22@gmail.com>
2025-04-10 02:12:48 -0400

    Proyecto de Slow Store

---
+++ SlowStore/Controllers/ProductoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SlowStore.DataContext;
+using SlowStore.Models;
+
+namespace SlowStore.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Mal rendimiento: carga todos los productos con ToList() y luego filtra
+            var productos = _context.Productos.ToList();
+
+            var productosCaros = new List<Producto>();
+            foreach (var p in productos)
+            {
+                if (p.Precio > 1000)
+                {
+                    productosCaros.Add(p); // lógica ineficiente
+                }
+            }
+
+            return View(productosCaros);
+        }
+    }
+}
---
+++ SlowStore/DataContext/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using SlowStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace SlowStore.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
